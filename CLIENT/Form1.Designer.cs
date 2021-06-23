16699161abf462dbc083f4d81143b993e0b37e20
vqlinhks123 <vuquanglinh2122001@gmail.com>
2021-06-23 23:40:08 +0700

    have bug when debug

--- CLIENT/Form1.Designer.cs
+++ CLIENT/Form1.Designer.cs
@@ -60,7 +60,7 @@
             button1.Location = new System.Drawing.Point(0, 0);
             button1.Margin = new System.Windows.Forms.Padding(0);
             button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(267, 154);
+            button1.Size = new System.Drawing.Size(200, 125);
             button1.TabIndex = 3;
             button1.Text = "  Online   dictionary";
             button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -68,11 +68,10 @@
             // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(293, 292);
-            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Location = new System.Drawing.Point(220, 237);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(935, 370);
+            this.webBrowser1.Size = new System.Drawing.Size(701, 301);
             this.webBrowser1.TabIndex = 11;
             // 
             // displayInput
@@ -81,10 +80,9 @@
             this.displayInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.displayInput.Enabled = false;
             this.displayInput.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.displayInput.Location = new System.Drawing.Point(293, 203);
-            this.displayInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.displayInput.Location = new System.Drawing.Point(220, 165);
             this.displayInput.Name = "displayInput";
-            this.displayInput.Size = new System.Drawing.Size(635, 52);
+            this.displayInput.Size = new System.Drawing.Size(476, 42);
             this.displayInput.TabIndex = 12;
             this.displayInput.Text = "";
             // 
@@ -97,10 +95,10 @@
             this.viewTypeBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.viewTypeBtn.ForeColor = System.Drawing.SystemColors.GrayText;
             this.viewTypeBtn.Image = ((System.Drawing.Image)(resources.GetObject("viewTypeBtn.Image")));
-            this.viewTypeBtn.Location = new System.Drawing.Point(0, 226);
+            this.viewTypeBtn.Location = new System.Drawing.Point(0, 184);
             this.viewTypeBtn.Margin = new System.Windows.Forms.Padding(0);
             this.viewTypeBtn.Name = "viewTypeBtn";
-            this.viewTypeBtn.Size = new System.Drawing.Size(267, 63);
+            this.viewTypeBtn.Size = new System.Drawing.Size(200, 51);
             this.viewTypeBtn.TabIndex = 5;
             this.viewTypeBtn.Text = "   View word type";
             this.viewTypeBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -115,10 +113,10 @@
             this.viewMeaningBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.viewMeaningBtn.ForeColor = System.Drawing.SystemColors.GrayText;
             this.viewMeaningBtn.Image = ((System.Drawing.Image)(resources.GetObject("viewMeaningBtn.Image")));
-            this.viewMeaningBtn.Location = new System.Drawing.Point(0, 154);
+            this.viewMeaningBtn.Location = new System.Drawing.Point(0, 125);
             this.viewMeaningBtn.Margin = new System.Windows.Forms.Padding(0);
             this.viewMeaningBtn.Name = "viewMeaningBtn";
-            this.viewMeaningBtn.Size = new System.Drawing.Size(267, 63);
+            this.viewMeaningBtn.Size = new System.Drawing.Size(200, 51);
             this.viewMeaningBtn.TabIndex = 4;
             this.viewMeaningBtn.Text = "  View meaning";
             this.viewMeaningBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -130,11 +128,10 @@
             this.tbMessage.BackColor = System.Drawing.Color.White;
             this.tbMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbMessage.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbMessage.Location = new System.Drawing.Point(612, 23);
-            this.tbMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbMessage.Location = new System.Drawing.Point(459, 19);
             this.tbMessage.Multiline = true;
             this.tbMessage.Name = "tbMessage";
-            this.tbMessage.Size = new System.Drawing.Size(227, 39);
+            this.tbMessage.Size = new System.Drawing.Size(170, 32);
             this.tbMessage.TabIndex = 1;
             // 
             // searchedList
@@ -144,10 +141,9 @@
             this.searchedList.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.searchedList.ForeColor = System.Drawing.SystemColors.MenuText;
             this.searchedList.HideSelection = false;
-            this.searchedList.Location = new System.Drawing.Point(0, 356);
-            this.searchedList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchedList.Location = new System.Drawing.Point(0, 289);
             this.searchedList.Name = "searchedList";
-            this.searchedList.Size = new System.Drawing.Size(267, 304);
+            this.searchedList.Size = new System.Drawing.Size(200, 247);
             this.searchedList.TabIndex = 3;
             this.searchedList.UseCompatibleStateImageBehavior = false;
             this.searchedList.View = System.Windows.Forms.View.List;
@@ -157,10 +153,9 @@
             this.searchBtn.FlatAppearance.BorderSize = 2;
             this.searchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.searchBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.searchBtn.Location = new System.Drawing.Point(855, 23);
-            this.searchBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.searchBtn.Location = new System.Drawing.Point(641, 19);
             this.searchBtn.Name = "searchBtn";
-            this.searchBtn.Size = new System.Drawing.Size(112, 39);
+            this.searchBtn.Size = new System.Drawing.Size(84, 32);
             this.searchBtn.TabIndex = 13;
             this.searchBtn.Text = "search";
             this.searchBtn.UseVisualStyleBackColor = true;
@@ -175,10 +170,9 @@
             this.panel1.Controls.Add(this.searchedList);
             this.panel1.Controls.Add(this.viewTypeBtn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(3, 2);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(267, 664);
+            this.panel1.Size = new System.Drawing.Size(200, 539);
             this.panel1.TabIndex = 14;
             // 
             // exportExcelBtn
@@ -190,10 +184,10 @@
             this.exportExcelBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.exportExcelBtn.ForeColor = System.Drawing.SystemColors.GrayText;
             this.exportExcelBtn.Image = ((System.Drawing.Image)(resources.GetObject("exportExcelBtn.Image")));
-            this.exportExcelBtn.Location = new System.Drawing.Point(0, 289);
+            this.exportExcelBtn.Location = new System.Drawing.Point(0, 235);
             this.exportExcelBtn.Margin = new System.Windows.Forms.Padding(0);
             this.exportExcelBtn.Name = "exportExcelBtn";
-            this.exportExcelBtn.Size = new System.Drawing.Size(267, 63);
+            this.exportExcelBtn.Size = new System.Drawing.Size(200, 51);
             this.exportExcelBtn.TabIndex = 6;
             this.exportExcelBtn.Text = "   Export to Excel";
             this.exportExcelBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
@@ -207,10 +201,9 @@
             this.panel2.Controls.Add(this.searchBtn);
             this.panel2.Controls.Add(this.tbMessage);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(270, 2);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(202, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(970, 89);
+            this.panel2.Size = new System.Drawing.Size(728, 72);
             this.panel2.TabIndex = 15;
             // 
             // label1
@@ -219,10 +212,9 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label1.Location = new System.Drawing.Point(-5, 31);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(-4, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(294, 28);
+            this.label1.Size = new System.Drawing.Size(234, 21);
             this.label1.TabIndex = 14;
             this.label1.Text = "       Originally created by team 3";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -232,21 +224,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(296, 129);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(222, 105);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(289, 62);
+            this.label2.Size = new System.Drawing.Size(230, 50);
             this.label2.TabIndex = 15;
             this.label2.Text = "Word details";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1243, 668);
+            this.ClientSize = new System.Drawing.Size(932, 543);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -254,9 +245,8 @@
             this.Controls.Add(this.webBrowser1);
             this.ForeColor = System.Drawing.Color.White;
             this.HelpButton = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Text = "Client";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
